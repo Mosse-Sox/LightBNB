3b3c2cc12fb2c579a094c2ec1bc8f486e5811bdd
Mosse-Sox <autumngreenie990@gmail.com>
2023-07-27 22:19:51 +0000

    adds functionality for creating a listing, creates a new listing when users submit their listing info

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -171,7 +171,6 @@ const getAllProperties = function (options, limit = 10) {
    `;
   }
 
-
   // 6
   return pool
     .query(queryString, queryParams)
@@ -189,10 +188,39 @@ const getAllProperties = function (options, limit = 10) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const cost_per_night_cents = property.cost_per_night * 100;
+  return pool
+    .query(
+      `INSERT INTO properties (owner_id, title, description, 
+    thumbnail_photo_url, cover_photo_url, cost_per_night, 
+    parking_spaces, number_of_bathrooms, number_of_bedrooms, 
+    country, street, city, province, post_code, active) 
+    VALUES ($1, $2, $3, 
+            $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, true) 
+    RETURNING *;`,
+      [
+        property.owner_id,
+        property.title,
+        property.description,
+        property.thumbnail_photo_url,
+        property.cover_photo_url,
+        cost_per_night_cents,
+        property.parking_spaces,
+        property.number_of_bathrooms,
+        property.number_of_bedrooms,
+        property.country,
+        property.street,
+        property.city,
+        property.province,
+        property.post_code,
+      ]
+    )
+    .then((result) => {
+      return console.log(result.rows);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 module.exports = {
